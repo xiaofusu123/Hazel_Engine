8bf860ac228a4b9735c362f584f0924086a72959
xiaofusu123 <qingfengfusu@foxmail.com>
2025-10-26 22:59:14 +0800

    按键代码映射。

--- hazel/src/Hazel.h
+++ hazel/src/Hazel.h
@@ -3,6 +3,11 @@
 #include "hazel/Application.h"
 #include "hazel/Layer.h"
 #include "hazel/Log.h"
+
+#include "hazel/Input.h"
+#include "hazel/KeyCodes.h"
+#include "hazel/MouseButtonCodes.h"
+
 #include "hazel/ImGui/ImGuiLayer.h"
 
 #include "hazel/EntryPoint.h"
